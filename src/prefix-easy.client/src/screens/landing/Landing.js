7b06d997663f4fdf5b496cf0516a1d53ddc7338e
jay-grant <jay.amgrant@gmail.com>
2018-03-24 16:26:36 +1000

    time for React, again

---
+++ src/prefix-easy.client/src/screens/landing/Landing.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import './landing.css';
+
+export class Landing extends React.Component {
+    render() {
+        return (
+            <div class="home-container">
+                <div class="home-content">
+
+                    <div class="title-block two-line-grid">
+                    <div class="left-cell row-one">PREFIX</div>
+                    <div class="right-cell row-one">EASY</div>
+                    <div class="left-cell row-two">prefiX</div>
+                    <div class="right-cell row-two">easy</div>
+                    </div>
+
+                </div>
+            </div>
+        );
+    }
+}
